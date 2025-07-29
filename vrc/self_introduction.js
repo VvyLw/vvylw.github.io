726390c35aec93b97a899bfb623533cb6b836576
VvyLw <owl1465fu9@gmail.com>
2025-07-29 10:05:47 +0900

    test

--- vrc/self_introduction.js
+++ vrc/self_introduction.js
@@ -2,11 +2,12 @@ document.querySelectorAll('.intro-img').forEach((img) => {
     img.addEventListener('click', () => {
         const modal = document.getElementById('img-modal');
         modal.classList.add('active');
-        modal.querySelector('img').src = this.src;
-        modal.querySelector('img').alt = this.alt;
+        const modalImg = modal.querySelector('img');
+        modalImg.src = img.src;
+        modalImg.alt = img.alt;
     });
 });
-document.getElementById('img-modal').addEventListener('click', (e) => this.classList.remove('active'));
+document.getElementById('img-modal').addEventListener('click', (e) => e.currentTarget.classList.remove('active'));
 
 const micBar = document.getElementById('micBar');
 const percentageText = document.getElementById('percentageText');
